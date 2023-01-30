e72d9e7a52ad008edd224c7c9820b0090403c2ab
Christoph Asam <asam.chr@gmail.com>
2023-01-30 14:49:44 +0100

    Set frame count by command line argument

--- plot_background_waves.py
+++ plot_background_waves.py
@@ -1,14 +1,14 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+from os import path
 
 # Convert frames to images with: ffmpeg -framerate 12 -pattern_type glob -i '*.jpg'   -c:v libx264 -pix_fmt yuv420p out.mp4
 
-def plot_wave(source):
+def plot_wave(source, number_of_frames):
     U_0 = np.load("./bs_test/U_0.npy")
     # print(f"Shape of U_0: {U_0.shape}")
 
-    number_of_frames = 3
     U_0 = np.reshape(U_0, (175, 350, 50, number_of_frames),order='F')
 
 
@@ -17,14 +17,17 @@ def plot_wave(source):
         plt.imshow(slice)
         plt.colorbar(orientation="horizontal")
         print(f"Write frame: {timestep+1}/{number_of_frames}")
-        plt.savefig(f'./wave_frames/frame_{timestep:03d}.jpg', dpi=300)
+        plt.savefig(f'wave_frames{ path.sep }frame_{timestep:03d}.jpg', dpi=300)
         plt.clf()
         plt.cla()
     #plt.show()
 
 if __name__ == "__main__":
     Ns = 25
+    n = 25
     for i, arg in enumerate(sys.argv):
         if arg == '-Ns':
             Ns = int(sys.argv[i+1])
-    plot_wave(Ns)
+        elif arg == '-n':
+            n = int(sys.argv[i+1])
+    plot_wave(Ns, n)
