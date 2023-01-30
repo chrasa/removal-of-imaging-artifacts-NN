66ce70e9b3c57bdf2a26ef8d45c8f15daf4880a5
Christoph Asam <asam.chr@gmail.com>
2023-01-30 14:46:34 +0100

    Plot data at receivers

---
+++ plot_receiver_data.py
@@ -0,0 +1,27 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from os import path
+
+def plot_mass_matrix(D_0_path, tx_sensor, rx_sensor):
+    D_0 = np.load(D_0_path)
+    print(f"Shape of M_0: {D_0.shape} (Time, Sensor, Sensor)")
+
+    plt.plot(D_0[:,tx_sensor,rx_sensor])
+    plt.title("Receiver Data")
+    plt.grid()
+    plt.show()
+
+if __name__ == "__main__":
+    D_0_path = "bs_test" + path.sep + "D_0.npy"
+    tx_sensor = 0
+    rx_sensor = 0
+
+    for i, arg in enumerate(sys.argv):
+        if arg == '-p':
+            D_0_path = sys.argv[i+1]
+        elif arg == '-tx':
+            tx_sensor = int(sys.argv[i+1])
+        elif arg == '-rx':
+            rx_sensor = int(sys.argv[i+1])
+    plot_mass_matrix(D_0_path, tx_sensor, rx_sensor)
