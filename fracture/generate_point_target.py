62c24d1d8c0207528d46acb7aa8158ae43fb9813
Christoph Asam <asam.chr@gmail.com>
2023-02-13 14:46:05 +0100

    Set position of target

--- fracture/generate_point_target.py
+++ fracture/generate_point_target.py
@@ -1,16 +1,16 @@
 import numpy as np
 
 
-def circle(width=512, height=512, c0=1000, c_circle=100):
+def circle(width=512, height=512, x=256, y=256, c0=1000, c_circle=100):
     xx, yy = np.mgrid[:width, :height]
-    circle = (xx - width/2) ** 2 + (yy - height/2) ** 2
+    circle = (xx - x) ** 2 + (yy - y) ** 2
 
     background = np.full((width, height), c0)
     circle = (circle < 100) * (c_circle - c0)
 
     return background + circle
 
-img = circle()
+img = circle(y=100)
 
 img = img.reshape((512*512,1))
 np.save(f"./images/circle.npy", img)
