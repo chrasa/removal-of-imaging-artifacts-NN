a3882d1defa9c941836781f35b40c09577bd59df
Christoph Asam <asam.chr@gmail.com>
2023-03-02 14:49:11 +0100

    Added class or cpu and gpu abstraction

--- RTM.py
+++ RTM.py
@@ -6,93 +6,66 @@ import scipy
 import cupy
 import cupyx
 
-from os.path import exists
-from os.path import sep
+from os import path
 
 from SimulationSetup import SimulationSetup
 from benchmark import timeit
-from typing import TypeVar
+
+from cpu_gpu_abstraction import CPU_GPU_Abstractor, ExecutionSetup
 
 PAST = 2
 PRESENT = 1
 FUTURE = 0
 
-Array = TypeVar('Array', numpy.array, cupy.array)
-
-class RTM:
-
-    def __init__(self, setup: SimulationSetup, gpu: bool=False):
-
-        self.setup_numpy_and_scipy(gpu)
+class RTM(CPU_GPU_Abstractor):
+    def __init__(self, setup: SimulationSetup, exec_setup: ExecutionSetup):
+        super(RTM, self).__init__(exec_setup=exec_setup)
 
         self.setup = setup
-        self.background_velocity = self.xp.full(self.setup.N**2, setup.background_velocity_value, dtype=self.xp.float64)
+        self.background_velocity = self.xp.full(self.setup.N**2, setup.background_velocity_value, dtype=self.exec_setup.precision)
         self.nts = 20
         self.delta_t = setup.tau/self.nts
-        self.memmap_order = 'C'
 
-        self.data_folder = "." + sep + "rtm_data" + sep    
-
-    def setup_numpy_and_scipy(self, gpu):
-        if cupy.cuda.runtime.getDeviceCount() > 0 and gpu:
-            self.xp = cupy
-            self.scipy = cupyx.scipy
-            self.gpu = True
-
-            device = cupy.cuda.Device(0)
-            print(f"GPU-Device ID: {device.id}")
-            print(f"GPU-Compute Capability: {device.compute_capability}")
-            print(f"GPU-Memory available: {device.mem_info[0]/1e6:.1f}/{device.mem_info[1]/1e6:.1f} MB")
-
-        else:
-            self.xp = numpy
-            self.scipy = scipy
-            self.gpu = False
+        self.data_folder = f".{path.sep}rtm_data{path.sep}"    
 
     def generate_sources(self):
         source_locations = numpy.array([15,  25,  35,  44,  54,  64,  74,  84,  93, 103, 113, 123, 133,
                     142, 152, 162, 172, 182, 191, 201, 211, 221, 231, 240, 250, 260,
                     270, 279, 289, 299, 309, 319, 328, 338, 348, 358, 368, 377, 387,
                     397, 407, 417, 426, 436, 446, 456, 466, 475, 485, 495])
-        B_delta = numpy.zeros([512*512, 50])
+        B_delta = numpy.zeros([512*512, 50], dtype=self.exec_setup.precision)
 
         for idx in range(len(source_locations)):
             B_delta[:,idx] = scipy.signal.unit_impulse(512*512, source_locations[idx])
         
-        if self.gpu:
-            return cupy.asarray(B_delta)
-        else:
-            return B_delta
+        return cupy.asarray(B_delta)
 
     @timeit 
     def get_A(self):
-        c = self.xp.full(self.setup.N**2, self.setup.background_velocity_value, dtype=self.xp.float64)
+        c = self.xp.full(self.setup.N**2, self.setup.background_velocity_value, dtype=self.exec_setup.precision)
         I_k = self.scipy.sparse.identity(self.setup.N)
 
-        D_k = (1/self.setup.delta_x**2)*self.scipy.sparse.diags([1,-2,1],[-1,0,1], shape=(self.setup.N,self.setup.N), dtype=self.xp.float64)
-        D_k = self.scipy.sparse.csr_matrix(D_k)
+        D_k = (1/self.setup.delta_x**2)*self.scipy.sparse.diags([1,-2,1],[-1,0,1], shape=(self.setup.N,self.setup.N), dtype=self.exec_setup.precision)
+        D_k = self.scipy.sparse.csr_matrix(D_k, dtype=self.exec_setup.precision)
         D_k[0, 0] = -1 * (1/self.setup.delta_x**2)
 
         L = self.scipy.sparse.kron(D_k, I_k) + self.scipy.sparse.kron(I_k, D_k)
-        C = self.scipy.sparse.diags(c, 0, dtype=self.xp.float64)
+        C = self.scipy.sparse.diags(c, 0, dtype=self.exec_setup.precision)
         A = (- C @ L @ C)
 
         return A
 
     @timeit
-    def calculate_U_RT(self, filename="U_RT.npy"):
-        # Load D
-        D_0 = self.xp.load(self.data_folder + "D_fine.npy")
+    def calculate_U_RT(self, D_file_name="D_fine.npy", U_RT_file_name="U_RT.npy"):
+        D = self.xp.load(self.data_folder + D_file_name)
 
         # Reverse the time 
-        D_0 = self.xp.flip(D_0, 0)
+        D = self.xp.flip(D, 0)
 
-        print(f"Shape of D_0: {D_0.shape}")
-        Nt = D_0.shape[0]
+        Nt = D.shape[0]
 
-        u = self.xp.zeros([3,512*512,self.setup.N_s])
-        # U = self.xp.zeros([int(Nt/self.nts),self.setup.N*self.setup.N])
-        U = numpy.memmap(self.data_folder + filename, numpy.float64, 'w+', shape=(int(Nt/self.nts),self.setup.N*self.setup.N, self.setup.N_s), order=self.memmap_order)
+        u = self.xp.zeros([3,512*512,self.setup.N_s], dtype=self.exec_setup.precision)
+        U = numpy.memmap(self.data_folder + U_RT_file_name, self.exec_setup.precision, 'w+', shape=(int(Nt/self.nts),self.setup.N*self.setup.N, self.setup.N_s))
 
         B_delta = self.generate_sources()
         A = self.get_A()
@@ -100,31 +73,17 @@ class RTM:
         factor = 2*self.scipy.sparse.identity(self.setup.N**2) - self.delta_t**2 * A
 
         for time_idx in range(Nt):
-            self.__print_benchmark_progress(time_idx+1, Nt)
+            self._print_progress_bar(time_idx+1, Nt)
             u[PAST,:,:] = u[PRESENT,:,:]
             u[PRESENT,:,:] = u[FUTURE,:,:]
-            u[FUTURE,:,:] = factor@u[PRESENT,:,:] - u[PAST,:,:] + self.delta_t**2 *  B_delta @ D_0[time_idx,:,:]
+            u[FUTURE,:,:] = factor@u[PRESENT,:,:] - u[PAST,:,:] + self.delta_t**2 *  B_delta @ D[time_idx,:,:]
 
             if (time_idx % self.nts) == 0:
-                if self.gpu:
-                    u_on_cpu = cupy.asnumpy(u[FUTURE,:,:])
-                    U[int(time_idx/self.nts),:,:] = u_on_cpu
-                else:
-                    U[int(time_idx/self.nts),:,:] = u[FUTURE,:,:]
-        
+                U[int(time_idx/self.nts),:,:] = cupy.asnumpy(u[FUTURE,:,:])
+
         sys.stdout.write("\n")
-        #self.xp.save("RTM_U.npy", U)
         U.flush()
         
-
-
-    def __print_benchmark_progress(self, progress, max, progress_bar_length=40):
-        title = f"\rRTM solver progress: {progress:5}/{max:5}: "
-        success_rate = f" {(progress/max)*100:3.2f}%"
-        number_of_progress_indicators = int(progress * progress_bar_length // (max))
-
-        sys.stdout.write(title + "[" + number_of_progress_indicators*"#" + (progress_bar_length - number_of_progress_indicators)*"-" + "]" + success_rate)
-
 
 def main():
     N_t = 70
@@ -137,7 +96,8 @@ def main():
             use_gpu = True
 
     sim_setup = SimulationSetup(N_t=N_t)
-    solver = RTM(sim_setup, use_gpu)
+    exec_setup = ExecutionSetup(gpu=use_gpu, precision='float64')
+    solver = RTM(sim_setup, exec_setup)
     solver.calculate_U_RT()
 
 
---
+++ cpu_gpu_abstraction.py
@@ -0,0 +1,64 @@
+import sys
+
+import numpy
+import scipy
+
+import cupy
+import cupyx
+
+import datetime
+from dataclasses import dataclass
+
+@dataclass
+class ExecutionSetup:
+    gpu: bool = False
+    precision: str = 'float64'
+
+
+class CPU_GPU_Abstractor:
+    def __init__(self, exec_setup: ExecutionSetup):
+        self.exec_setup = exec_setup
+        self.__setup_numpy_and_scipy()
+        self.__setup_precision()
+
+        self.__pb_started = False
+
+    def __setup_precision(self):
+        if self.exec_setup.precision == 'float32':
+            self.exec_setup.precision = self.xp.float32
+        elif self.exec_setup.precision == 'float64':
+            self.exec_setup.precision = self.xp.float64
+        else:
+            raise Exception("Invalid floating point precision specified")
+
+    def __setup_numpy_and_scipy(self):
+        if cupy.cuda.runtime.getDeviceCount() > 0 and self.exec_setup.gpu:
+            self.xp = cupy
+            self.scipy = cupyx.scipy
+            self.gpu = True
+
+            device = cupy.cuda.Device(0)
+            print(f"GPU-Device ID: {device.id}")
+            print(f"GPU-Compute Capability: {device.compute_capability}")
+            print(f"GPU-Memory available: {device.mem_info[0]/1e6:.1f}/{device.mem_info[1]/1e6:.1f} MB")
+
+        else:
+            self.xp = numpy
+            self.scipy = scipy
+            self.gpu = False
+
+    def _print_progress_bar(self, progress, max, progress_bar_length=40, title="Progress"):
+        progress_ratio = progress/max
+        if not self.__pb_started:
+            self.__ts = datetime.datetime.now()
+            self.__pb_started = True
+
+        tc = datetime.datetime.now()
+        tf = (1/progress_ratio -1)*(tc - self.__ts).total_seconds()
+
+        title = f"\r{title}: {progress:5}/{max:5}: "
+        success_rate = f" {progress_ratio*100:3.2f}%"
+        remaining_time = f" ERT: {int(tf/60):02}:{int(tf%60):02}"
+        number_of_progress_indicators = int(progress * progress_bar_length // (max))
+
+        sys.stdout.write(title + "[" + number_of_progress_indicators*"#" + (progress_bar_length - number_of_progress_indicators)*"-" + "]" + success_rate + remaining_time)
