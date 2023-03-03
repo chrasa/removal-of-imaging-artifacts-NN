f3f41a679dfa1709dc0e99c8af4b6147b08f6bcf
Christoph Asam <asam.chr@gmail.com>
2023-03-03 17:39:11 +0100

    Use parameters definded in FractureSetup

--- plot_fracture.py
+++ plot_fracture.py
@@ -1,21 +1,26 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 from os import path
+from fracture.FractureSetup import FractureSetup
 
-def plot_fracture(fracture_path, image_height:int=512, image_width:int=512):
+def plot_fracture(fracture_path, frac_setup: FractureSetup):
     image = np.load(fracture_path)
-
-    image = image.reshape(image_height, image_width, 1)
-    plt.gray()
-    plt.title(f"{fracture_path}")
-    plt.imshow(np.squeeze(image))
+    image = image.reshape(frac_setup.image_height, frac_setup.image_width, 1)
+    
+    fig, ax = plt.subplots()
+    ax.set_title(f"{fracture_path}")
+    ax.imshow(np.squeeze(image))
+    rect = patches.Rectangle((frac_setup.O_x, frac_setup.O_y), frac_setup.fractured_region_width, frac_setup.fractured_region_height, linewidth=1, edgecolor='r', facecolor='none')
+    ax.add_patch(rect)
     plt.show()
 
 if __name__ == "__main__":
     fracture_path = "fracture" + path.sep + "images" + path.sep + "im0.npy"
+    fracture_setup = FractureSetup()
 
     for i, arg in enumerate(sys.argv):
         if arg == '-p':
             fracture_path = sys.argv[i+1]
-    plot_fracture(fracture_path)
+    plot_fracture(fracture_path, fracture_setup)
