460b0ec247ca217ce48bf75f0b73b97165eeddd8
Christoph Asam <asam.chr@gmail.com>
2023-03-31 13:10:47 +0200

    Remove old comments

--- ROM.py
+++ ROM.py
@@ -1,13 +1,8 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy import sparse
 from cholesky import mblockchol
-import scipy as sp
-import cupyx
-import cupy as cp
 from scipy import ndimage
 from os.path import exists
-from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 class ROM:
 
@@ -114,47 +109,7 @@ class ROM:
         return ind_list
 
     def calculate_u_d(self, u, A, D, b):
-        # Make different function for D calculate_u_d
-        # Discretize time
-        # nts = 20
-        # T = (self.N_t * 2 - 1) * self.delta_t * nts
-        # time = np.linspace(0, T, num=2*self.N_t*nts)
-
-        # U_0 = np.zeros((self.N_x_im*self.N_y_im, self.N_s, self.N_t))
-        # U_0[:,:,0] = u[1][self.imaging_region_indices]
-        
-        # count_storage_D = 0
-        # count_storage_U_0 = 0
-        # for i in range(1,len(time)):
-        #     u[2] = u[1] 
-        #     u[1] = u[0] 
-        #     u[0] = (-self.delta_t**2 * A) @ u[1] - u[2] + 2*u[1]
-
-        #     if (i % nts) == 0:
-        #         index = int(i/nts)
-        #         D[index] = np.transpose(b) @ u[1]
-        #         D[index] = 0.5*(D[index].T + D[index])
-
-        #         count_storage_D += 1
-        #         print(f"{count_storage_D}/{len(time)/nts}")
-
-        #         if i <= self.N_t*nts-1:
-        #             U_0[:,:,index] = u[1][self.imaging_region_indices]
-
-        #             count_storage_U_0 += 1
-
-        # print("reshape U_0")
-        # U_0 = np.reshape(U_0, (self.N_x_im * self.N_y_im, self.N_s * self.N_t),order='F')
-        # np.save("./U_0.npy", U_0)
-
-        # print(f"Count D = {count_storage_D}")
-        # print(f"Count stage U_0 = {count_storage_U_0}")
-
         D = np.load("./rtm_data/D_0.npy")
-        # U_0_mem = np.memmap("../rtm_data/U_0.npy", np.float64, 'r', shape=(2*self.N_t, self.N_y_im*self.N_x_im, self.N_s))
-
-        # U_0 = np.array(U_0_mem[0:self.N_t, :, :])
-        # U_0 = np.reshape(U_0, (self.N_x_im * self.N_y_im, self.N_s * self.N_t),order='F')
 
         U_0_mem = np.memmap("./rtm_data/U_0.npy", np.float64, 'r', shape=(2*self.N_t, self.N_y_im*self.N_x_im, self.N_s))
         U_0 = np.array(U_0_mem)
@@ -165,30 +120,7 @@ class ROM:
         return D, U_0
 
     def calculate_d(self, u, A, D, b):
-        # nts = 20
-        # T = (self.N_t * 2 - 1) * self.delta_t * nts
-        # time = np.linspace(0, T, num=2*self.N_t*nts)
-        
-        # count_storage_D = 0
-        # for i in range(1,len(time)):
-        #     u[2] = u[1] 
-        #     u[1] = u[0] 
-        #     u[0] = (-self.delta_t**2 * A) @ u[1] - u[2] + 2*u[1]
-
-        #     if (i % nts) == 0:
-        #         index = int(i/nts)
-        #         D[index] = np.transpose(b) @ u[1]
-        #         D[index] = 0.5*(D[index].T + D[index])
-
-        #         count_storage_D += 1
-        #         print(count_storage_D)
-
-        # print(f"Count D = {count_storage_D}")
-
-        # np.save("./D.npy", D)
-
         D = np.load("./rtm_data/D.npy")
-        print(D.shape)
         return D
 
     def calculate_intensity(self, C: np.array):
@@ -215,10 +147,6 @@ class ROM:
         return R
     
     def calculate_imaging_func(self, R):
-        """
-        # for i in range(self.N_x_im*self.N_y_im):
-        #     I[i] = np.linalg.norm(V_0[i, :] @ R, 2)**2
-        """
         I = self.V_0 @ R
         I = np.square(I)
 
@@ -234,63 +162,10 @@ class ROM:
 
         return I_x
     
-    def plot_intensity(self, I, plot_title):
-        data_temp = np.reshape(I, (self.N_y_im, self.N_x_im))
-
-        self.plot_result_matrix(data_temp, plot_title, np.shape(data_temp)[1], np.shape(data_temp)[0])
-
     def calculate_I_matrices(self, n_images: int, plot: bool, output_file: str = ""):
         c = np.load(f"./old_fwd/fractures/circle.npy")
         I = self.calculate_intensity(c)
         np.save("./rom_data/I.npy", I)
-        # I = np.load("./I.npy")
-        I = np.squeeze(I)
-        
-        if exists("./rom_data/I_0.npy"):
-            I_0 = np.load("./rom_data/I_0.npy")
-        else:
-            R = np.load("./rom_data/R_0.npy")
-            I_0 = self.calculate_imaging_func(R)
-        
-        I = I - I_0
-        I = self.get_image_derivative(I)
-
-        if plot:
-            self.plot_intensity(I, f'im1')
-
-        if output_file:
-            np.save(output_file, I)
-
-    def plot_result_matrix(self,
-                        matrix_results,
-                        matrix_name: str='matrix_results',
-                        x_dim: int= 175,
-                        y_dim: int= 150):
-
-        fig, ax = plt.subplots()
-        im = ax.imshow(ndimage.rotate(np.squeeze(matrix_results), -90), aspect = 'equal', cmap='Greys') 
-        plt.title(f"Colormap of matrix {matrix_name}")
-        plt.xlabel("Coordinate in y-dim")
-        plt.ylabel("Coordinate in x-dim")
-        plt.xlim(0, y_dim)
-        plt.ylim(0, x_dim)
-        divider = make_axes_locatable(ax)
-        cax = divider.append_axes('right', size='5%', pad=0.1)
-        fig.colorbar(im, cax=cax, orientation='vertical')
-        ax.invert_yaxis()
-        
-        plt.show()
-
-    def plot_samples_of_V0(self):
-        V0 = np.load("V_0.npy")
-        samples = [2500, 2547]
-        #for i in range(0, 3500, 50):
-        for i in samples:
-            fig, ax = plt.subplots()
-            im = ax.imshow(ndimage.rotate(np.reshape(V0[:, i], (self.N_y_im, self.N_x_im)), -90), aspect = 'equal', cmap='Greys') 
-            fig.colorbar(im)
-            plt.title(f"Colormap of V0[:, {i}]")
-        plt.show()
 
 
 def main():
@@ -310,19 +185,6 @@ def main():
     )
 
     solver.calculate_I_matrices(10, True, False)
-    # solver.calculate_intensities()
-    #I = np.load("./I_result.npy")
-    # V0 = np.load("./V0.npy")
-    #solver.plot_intensity(I)
-
-    # solver.plot_samples_of_V0()
-
-    # I = np.load("./I.npy")
-    # I = np.squeeze(I)
-    # I_0 = np.load("./I_0.npy")
-
-    # I = I - I_0
-    # solver.plot_intensity(I, "Test")
 
 if __name__ == "__main__":
     main()
