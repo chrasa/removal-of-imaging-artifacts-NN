77a4736d694fff373aa00fbc1f8ed2898b123160
Christoph Asam <asam.chr@gmail.com>
2023-03-31 14:03:34 +0200

    Use CPU GPU abstraction

--- ROM.py
+++ ROM.py
@@ -5,36 +5,39 @@ from scipy import ndimage
 from os.path import exists
 from SimulationSetup import SimulationSetup
 from benchmark import timeit
+from cpu_gpu_abstraction import CPU_GPU_Abstractor, ExecutionSetup
 
-class ROM:
 
-    def __init__(self, setup: SimulationSetup = SimulationSetup()):
+class ROM(CPU_GPU_Abstractor):
+
+    def __init__(self, setup: SimulationSetup, exec_setup: ExecutionSetup):
+        super(ROM, self).__init__(exec_setup=exec_setup)
 
         self.setup = setup
 
         self.imaging_region_indices = self.get_imaging_region_indices()
 
         self.background_velocity_value = setup.background_velocity_value
-        self.background_velocity = np.full(self.setup.N**2, setup.background_velocity_value, dtype=np.float64)
+        self.background_velocity = self.xp.full(self.setup.N**2, setup.background_velocity_value, dtype=self.exec_setup.precision)
 
         self.delta_t = setup.tau/20
         
         if exists("./rom_data/V_0.npy"):
-            self.V_0 = np.load("./rom_data/V_0.npy")
+            self.V_0 = self.xp.load("./rom_data/V_0.npy")
         
         else:
             self.V_0 = self.calculate_V0()
-            np.save("./rom_data/V_0.npy", self.V_0)
+            self.xp.save("./rom_data/V_0.npy", self.V_0)
         
         if not exists("./rom_data/I_0.npy"):
-            R = np.load("./rom_data/R_0.npy")
+            R = self.xp.load("./rom_data/R_0.npy")
             I_0 = self.calculate_imaging_func(R)
-            np.save("./rom_data/I_0.npy", I_0)
+            self.xp.save("./rom_data/I_0.npy", I_0)
 
     def import_sources(self):
         
-        b = np.loadtxt(self.setup.Bsrc_file, delimiter =',', dtype=np.float64)
-        np.reshape(b, (self.setup.N_x * self.setup.N_y, self.setup.N_s))
+        b = self.xp.loadtxt(self.setup.Bsrc_file, delimiter =',', dtype=self.exec_setup.precision)
+        self.xp.reshape(b, (self.setup.N_x * self.setup.N_y, self.setup.N_s))
 
         return b
     
@@ -46,27 +49,27 @@ class ROM:
         return indices
         
     @timeit
-    def init_simulation(self, c: np.array):
+    def init_simulation(self, c):
         # Calculate operators
         I_k = sparse.identity(self.setup.N)
-        D_k = (1/self.setup.delta_x**2)*sparse.diags([1,-2,1],[-1,0,1], shape=(self.setup.N,self.setup.N), dtype=np.float64)
+        D_k = (1/self.setup.delta_x**2)*sparse.diags([1,-2,1],[-1,0,1], shape=(self.setup.N,self.setup.N), dtype=self.exec_setup.precision)
         D_k = sparse.csr_matrix(D_k)
         D_k[0, 0] = -1 * (1/self.setup.delta_x**2)
 
         L = sparse.kron(D_k, I_k) + sparse.kron(I_k, D_k)
-        C = sparse.diags(c, 0, dtype=np.float64)
+        C = sparse.diags(c, 0, dtype=self.exec_setup.precision)
 
         A = (- C @ L @ C)
 
-        u = np.zeros((3, self.setup.N_x * self.setup.N_y, self.setup.N_s), dtype=np.float64) # Stores past, current and future instances
+        u = self.xp.zeros((3, self.setup.N_x * self.setup.N_y, self.setup.N_s), dtype=self.exec_setup.precision) # Stores past, current and future instances
 
         b = self.import_sources()
 
         u[1] = b
         u[0] = (-0.5* self.delta_t**2 * A) @ b + b
 
-        D = np.zeros((2*self.setup.N_t, self.setup.N_s, self.setup.N_s), dtype=np.float64)
-        D[0] = np.transpose(b) @ u[1]
+        D = self.xp.zeros((2*self.setup.N_t, self.setup.N_s, self.setup.N_s), dtype=self.exec_setup.precision)
+        D[0] = self.xp.transpose(b) @ u[1]
 
         return u, A, D, b 
 
@@ -75,35 +78,35 @@ class ROM:
         u_init, A, D_init, b = self.init_simulation(self.background_velocity)
         D, U_0 = self.calculate_u_d(u_init, A, D_init, b) 
         R = self.calculate_mass_matrix(D)
-        V_0 = U_0 @ np.linalg.inv(R)
+        V_0 = U_0 @ self.xp.linalg.inv(R)
 
         if not exists("./rom_data/R_0.npy"):
-            np.save("./rom_data/R_0.npy", R)
+            self.xp.save("./rom_data/R_0.npy", R)
 
         return V_0
 
     def find_indices(self,j):
-        ind_t = np.linspace(0, self.setup.N_s, self.setup.N_s) + self.setup.N_s*j 
+        ind_t = self.xp.linspace(0, self.setup.N_s, self.setup.N_s) + self.setup.N_s*j 
         ind_list = [int(x) for x in ind_t]
         return ind_list
 
     def calculate_u_d(self, u, A, D, b):
-        D = np.load("./rtm_data/D_0.npy")
+        D = self.xp.load("./rtm_data/D_0.npy")
 
-        U_0_mem = np.memmap("./rtm_data/U_0.npy", np.float64, 'r', shape=(2*self.setup.N_t, self.setup.N_y_im*self.setup.N_x_im, self.setup.N_s))
-        U_0 = np.array(U_0_mem)
+        U_0_mem = self.xp.memmap("./rtm_data/U_0.npy", self.exec_setup.precision, 'r', shape=(2*self.setup.N_t, self.setup.N_y_im*self.setup.N_x_im, self.setup.N_s))
+        U_0 = self.xp.array(U_0_mem)
         U_0 = U_0[0:self.setup.N_t,:,:]
-        U_0 = np.moveaxis(U_0, 0, -1)
-        U_0 = np.reshape(U_0, (self.setup.N_x_im * self.setup.N_y_im, self.setup.N_s*self.setup.N_t), order='F')
+        U_0 = self.xp.moveaxis(U_0, 0, -1)
+        U_0 = self.xp.reshape(U_0, (self.setup.N_x_im * self.setup.N_y_im, self.setup.N_s*self.setup.N_t), order='F')
         
         return D, U_0
 
     def calculate_d(self, u, A, D, b):
-        D = np.load("./rtm_data/D.npy")
+        D = self.xp.load("./rtm_data/D.npy")
         return D
 
     @timeit
-    def calculate_intensity(self, C: np.array):
+    def calculate_intensity(self, C ):
         u_init, A_init, D_init, b = self.init_simulation(C)
         D = self.calculate_d(u_init, A_init, D_init, b)
         R = self.calculate_mass_matrix(D)
@@ -113,7 +116,7 @@ class ROM:
 
     @timeit
     def calculate_mass_matrix(self, D):
-        M = np.zeros((self.setup.N_s*self.setup.N_t, self.setup.N_s*self.setup.N_t), dtype=np.float64)
+        M = self.xp.zeros((self.setup.N_s*self.setup.N_t, self.setup.N_s*self.setup.N_t), dtype=self.exec_setup.precision)
 
         for i in range(self.setup.N_t):
             for j in range(self.setup.N_t):
@@ -129,7 +132,7 @@ class ROM:
     @timeit
     def calculate_imaging_func(self, R):
         I = self.V_0 @ R
-        I = np.square(I)
+        I = self.xp.square(I)
 
         I = I.sum(axis=1)
         
@@ -137,16 +140,16 @@ class ROM:
 
     def get_image_derivative(self, I):
         I = I.reshape((self.setup.N_y_im, self.setup.N_x_im))
-        dx = -1 * np.array([[-1, 0, 1]])
+        dx = -1 * self.xp.array([[-1, 0, 1]])
         I_x = ndimage.convolve(I, dx)
         I_x = I_x.reshape((-1, 1))
 
         return I_x
     
     def calculate_I_matrices(self, n_images: int, plot: bool, output_file: str = ""):
-        c = np.load(f"./old_fwd/fractures/circle.npy")
+        c = self.xp.load(f"./old_fwd/fractures/circle.npy")
         I = self.calculate_intensity(c)
-        np.save("./rom_data/I.npy", I)
+        self.xp.save("./rom_data/I.npy", I)
 
 
 def main():
@@ -158,7 +161,13 @@ def main():
         O_y=180
     )
 
-    solver = ROM(setup)
+    exec_setup = ExecutionSetup(
+        gpu=False,
+        precision='float64',
+        data_folder='rom_data'
+    )
+
+    solver = ROM(setup, exec_setup)
 
     solver.calculate_I_matrices(10, True, False)
 
