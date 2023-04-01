9cc74fc37622e7af108e05efa1ecae4887b00d76
Christoph Asam <asam.chr@gmail.com>
2023-04-01 10:30:58 +0200

    Calculate I_0 on init

--- RTM.py
+++ RTM.py
@@ -26,6 +26,21 @@ class RTM(CPU_GPU_Abstractor):
         self.nts = 20
         self.delta_t = setup.tau/self.nts
         self.imaging_region_indices = self.get_imaging_region_indices()
+        self.I_0_path = self.exec_setup.data_folder + "I_0_RTM.npy"
+
+        self.I_0 = self.get_I0()
+
+    def get_I0(self):
+        if path.exists(self.I_0_path):
+            return self.xp.load(self.I_0_path)
+        else:
+            I_0 = self.__calculate_I0()
+            self.xp.save(self.I_0_path, I_0)
+            return I_0
+        
+    def __calculate_I0(self):
+        self.calculate_U_RT(D_file_name="D_0_fine.npy", U_RT_file_name="U_0_RT.npy")
+        return self.__calculate_imaging_function(U_RT_file_name="U_0_RT.npy", I_file_name="I_0_RTM.npy")
 
     def generate_sources(self):
         source_locations = numpy.array([15,  25,  35,  44,  54,  64,  74,  84,  93, 103, 113, 123, 133,
@@ -84,7 +99,7 @@ class RTM(CPU_GPU_Abstractor):
         factor = 2*self.scipy.sparse.identity(self.setup.N**2) - self.delta_t**2 * A
 
         for time_idx in range(2*self.setup.N_t * self.nts):
-            self._print_progress_bar(time_idx+1, 2*self.setup.N_t*self.nts)
+            self._print_progress_bar(time_idx+1, 2*self.setup.N_t*self.nts, title="Reverse time calculation")
             u[PAST,:,:] = u[PRESENT,:,:]
             u[PRESENT,:,:] = u[FUTURE,:,:]
             u[FUTURE,:,:] = factor@u[PRESENT,:,:] - u[PAST,:,:] + self.delta_t**2 *  B_delta @ D[time_idx,:,:]
@@ -95,15 +110,14 @@ class RTM(CPU_GPU_Abstractor):
         self._end_progress_bar()
         U.flush()
 
-    @timeit
-    def calculate_I(self, U_RT_file_name="U_RT.npy", U_0_file_name="U_0.npy", I_file_name="I.npy"):
+    def __calculate_imaging_function(self, U_RT_file_name="U_RT.npy", U_0_file_name="U_0.npy", I_file_name=""):
         U_RT = numpy.memmap(self.exec_setup.data_folder + U_RT_file_name, self.exec_setup.precision_np, 'r', shape=(2*self.setup.N_t, self.setup.N_x_im*self.setup.N_y_im, self.setup.N_s))
         U_0 = numpy.memmap(self.exec_setup.data_folder + U_0_file_name, self.exec_setup.precision_np, 'r', shape=(2*self.setup.N_t, self.setup.N_x_im*self.setup.N_y_im, self.setup.N_s))
 
         I = self.xp.zeros((self.setup.N_x_im*self.setup.N_y_im, self.setup.N_s), dtype=self.exec_setup.precision)
 
         for tidx in range(2*self.setup.N_t):
-            self._print_progress_bar(tidx+1, 2*self.setup.N_t, title="Integral progress")
+            self._print_progress_bar(tidx+1, 2*self.setup.N_t, title="RTM Integral progress")
             t_idx_backward = 2*self.setup.N_t -tidx -1
             t_idx_forward = tidx
 
@@ -111,16 +125,19 @@ class RTM(CPU_GPU_Abstractor):
             U_0_temp = self.xp.array(U_0[t_idx_backward,:,:])
 
             for s_idx in range(self.setup.N_s):
-                temp = U_RT_temp[:,s_idx]
-                # temp = temp.reshape(512,512)
-                # temp = temp.T
-                # temp = temp.reshape(512*512)
-
-                I[:,s_idx] += temp * U_0_temp[:,s_idx] * self.setup.tau
+                I[:,s_idx] += U_RT_temp[:,s_idx] * U_0_temp[:,s_idx] * self.setup.tau
         self._end_progress_bar()
 
-        I = self.xp.sum(I, axis=1)
-        self.xp.save(self.exec_setup.data_folder + I_file_name, I)
+        return self.xp.sum(I, axis=1)
+    
+    @timeit
+    def calculate_I(self, U_RT_file_name="U_RT.npy", U_0_file_name="U_0.npy", I_file_name=""):
+        I = self.__calculate_imaging_function(U_RT_file_name, U_0_file_name, I_file_name)
+        I = I - self.I_0
+        if I_file_name:
+            self.xp.save(self.exec_setup.data_folder + I_file_name, I)
+        
+        return I
         
 
 def main():
@@ -133,9 +150,7 @@ def main():
     sim_setup = SimulationSetup(N_t=35)
     exec_setup = ExecutionSetup(gpu=use_gpu, precision='float32')
     solver = RTM(sim_setup, exec_setup)
-    # solver.calculate_U_RT(D_file_name="D_0_fine.npy", U_RT_file_name="U_0_RT.npy")
-    # solver.calculate_U_RT(D_file_name="D_fine.npy", U_RT_file_name="U_RT.npy")
-    solver.calculate_I(U_RT_file_name="U_0_RT.npy", I_file_name="I_0.npy")
+    solver.calculate_U_RT(D_file_name="D_fine.npy", U_RT_file_name="U_RT.npy")
     solver.calculate_I(U_RT_file_name="U_RT.npy", I_file_name="I.npy")
 
 
