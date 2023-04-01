4534ff1a7d93d214d9225b8a98f7f23a83201fe7
Christoph Asam <asam.chr@gmail.com>
2023-04-01 16:22:48 +0200

    Renamed WaveSolver class

--- WaveSolver.py
+++ WaveSolver.py
@@ -1,53 +1,35 @@
 import sys
+from os import path
 
 import numpy
-import scipy
-
 import cupy
-import cupyx
 
-from os.path import exists
-from os.path import sep
-
-from SimulationSetup import SimulationSetup
 from benchmark import timeit
+from cpu_gpu_abstraction import CPU_GPU_Abstractor, ExecutionSetup
+from SimulationSetup import SimulationSetup
+
 from typing import TypeVar
+
+PAST = 2
+PRESENT = 1
+FUTURE = 0
 
 Array = TypeVar('Array', numpy.array, cupy.array)
 
-class WaveSolver:
+class WaveSolver(CPU_GPU_Abstractor):
 
-    def __init__(self, setup: SimulationSetup, gpu: bool=False):
-
-        self.setup_numpy_and_scipy(gpu)
+    def __init__(self, setup: SimulationSetup, exec_setup: ExecutionSetup):
+        super(WaveSolver, self).__init__(exec_setup=exec_setup)
 
         self.setup = setup
 
         self.imaging_region_indices = self.get_imaging_region_indices()
-        self.background_velocity = self.xp.full(self.setup.N**2, setup.background_velocity_value, dtype=self.xp.float64)
+        self.background_velocity = self.xp.full(self.setup.N**2, setup.background_velocity_value, dtype=self.exec_setup.precision)
         self.delta_t = setup.tau/20
-
-        self.data_folder = "." + sep + "bs_test" + sep    
-
-    def setup_numpy_and_scipy(self, gpu):
-        if cupy.cuda.runtime.getDeviceCount() > 0 and gpu:
-            self.xp = cupy
-            self.scipy = cupyx.scipy
-            self.gpu = True
-
-            device = cupy.cuda.Device(0)
-            print(f"GPU-Device ID: {device.id}")
-            print(f"GPU-Compute Capability: {device.compute_capability}")
-            print(f"GPU-Memory available: {device.mem_info[0]/1e6:.1f}/{device.mem_info[1]/1e6:.1f} MB")
-
-        else:
-            self.xp = numpy
-            self.scipy = scipy
-            self.gpu = False
 
     def import_sources(self):
         
-        b = self.xp.loadtxt(self.setup.Bsrc_file, delimiter =',', dtype=self.xp.float64)
+        b = self.xp.loadtxt(self.setup.Bsrc_file, delimiter =',', dtype=self.exec_setup.precision)
         self.xp.reshape(b, (self.setup.N_x * self.setup.N_y, self.setup.N_s))
 
         return b
@@ -65,7 +47,7 @@ class WaveSolver:
         I_k = self.scipy.sparse.identity(self.setup.N)
 
         # D_k is the N_x × N_y tridiagonal matrix which represents the boundary conditions. The D_k matrix is presented in Equation 6
-        D_k = (1/self.setup.delta_x**2)*self.scipy.sparse.diags([1,-2,1],[-1,0,1], shape=(self.setup.N,self.setup.N), dtype=self.xp.float64)
+        D_k = (1/self.setup.delta_x**2)*self.scipy.sparse.diags([1,-2,1],[-1,0,1], shape=(self.setup.N,self.setup.N), dtype=self.exec_setup.precision)
         D_k = self.scipy.sparse.csr_matrix(D_k)
         D_k[0, 0] = -1 * (1/self.setup.delta_x**2)
 
@@ -73,114 +55,101 @@ class WaveSolver:
         L = self.scipy.sparse.kron(D_k, I_k) + self.scipy.sparse.kron(I_k, D_k)
 
         # C is a diagonal matrix of size N_x N_y × N_x N_y, which stores background velocity of the medium
-        C = self.scipy.sparse.diags(c, 0, dtype=self.xp.float64)
+        C = self.scipy.sparse.diags(c, 0, dtype=self.exec_setup.precision)
 
         # Equation 7
         A = (- C @ L @ C)
 
         # Equation 8
-        u = self.xp.zeros((3, self.setup.N_x * self.setup.N_y, self.setup.N_s), dtype=self.xp.float64) # Stores past, current and future instances
+        u = self.xp.zeros((3, self.setup.N_x * self.setup.N_y, self.setup.N_s), dtype=self.exec_setup.precision) # Stores past, current and future instances
 
         b = self.import_sources()
 
         # Equation 9
-        u[1] = b
-        u[0] = (-0.5* self.delta_t**2 * A) @ b + b
+        u[PRESENT] = b
+        u[FUTURE] = (-0.5* self.delta_t**2 * A) @ b + b
 
         # Equation 10
-        D = self.xp.zeros((2*self.setup.N_t, self.setup.N_s, self.setup.N_s), dtype=self.xp.float64)
+        D = self.xp.zeros((2*self.setup.N_t, self.setup.N_s, self.setup.N_s), dtype=self.exec_setup.precision)
         D[0] = self.xp.transpose(b) @ u[1]
 
         return u, A, D, b 
 
     @timeit
-    def calculate_U0_and_D0(self):
-        """Calculate the orthogonal background snapshots V_0"""
-        u_init, A, D_init, b = self.init_simulation(self.background_velocity)
-        D_0, U_0 = self.calculate_U_D(u_init, A, D_init, b)
+    def calculate_U_D(self, path_to_c):
+        """Calculate wave fields in the medium and the data at the receivers for a given fracture"""
+        c = self.xp.load(path_to_c)
+        c = self.xp.squeeze(c)
 
-        self.xp.save(self.data_folder + "D_0.npy", D_0)
-        self.xp.save(self.data_folder + "U_0.npy", U_0)
+        u_init, A, D_init, b = self.init_simulation(c)
+        D, D_fine = self.__calculate_U_D(u_init, A, D_init, b)
 
-        return D_0, U_0
+        self.xp.save(self.exec_setup.data_folder + "D.npy", D)
+        self.xp.save(self.exec_setup.data_folder + "D_fine.npy", D_fine)
 
+        return D, D_fine
+    
     @timeit
-    def calculate_U_D(self, u, A, D, b):
-        """Calculate wave fields in the medium and the data at the receivers
+    def calculate_U0_D0(self, file_name="U_0.npy"):
+        """Calculate wave fields in the medium and the data at the receivers without any fractures"""
 
-        Solve the wave equation for a reference wave speed c0 without any fractures by using a finite-
-        difference time domain method and collect the matrices U_0 (the wave field snapshots in the refer-
-        ence medium) and D_0 (the data measured at the receivers).
-        """
+        u_init, A, D_init, b = self.init_simulation(self.background_velocity)
+        D_0, D_0_fine = self.__calculate_U_D(u_init, A, D_init, b, file_name)
+
+        self.xp.save(self.exec_setup.data_folder + "D_0.npy", D_0)
+        self.xp.save(self.exec_setup.data_folder + "D_0_fine.npy", D_0_fine)
+
+
+    def __calculate_U_D(self, u, A, D, b, U_file_name="U.npy"):
         nts = 20
+        D_fine = self.xp.zeros((2*self.setup.N_t*nts, self.setup.N_s, self.setup.N_s), dtype=self.exec_setup.precision)
         T = (self.setup.N_t * 2 - 1) * self.delta_t * nts
         time = self.xp.linspace(0, T, num=2*self.setup.N_t*nts)
 
-        U_0 = self.xp.zeros((self.setup.N_x_im*self.setup.N_y_im, self.setup.N_s, self.setup.N_t))   # Can Fortran ordering be used already here?
-        U_0[:,:,0] = u[1][self.imaging_region_indices]      # Check if using a (sparse) projection matrix is faster?
+        U_0 = numpy.memmap(self.exec_setup.data_folder+U_file_name, self.exec_setup.precision_np, 'w+', shape=(2*self.setup.N_t, self.setup.N_y_im*self.setup.N_x_im, self.setup.N_s))
+        U_0[0,:,:] = cupy.asnumpy(u[PRESENT][self.imaging_region_indices])      # Check if using a (sparse) projection matrix is faster?
         
-        count_storage_D = 0
-        count_storage_U_0 = 0
         for i in range(1,len(time)):
-            self.__print_benchmark_progress(i+1, len(time))
-            u[2] = u[1] 
-            u[1] = u[0] 
-            #ts = ti.time() 
-            u[0] = (-self.delta_t**2 * A) @ u[1] - u[2] + 2*u[1]
-            #te = ti.time()
-            #print(f'Function:if Execution time:{(te -ts):10.2f}s')
+            self._print_progress_bar(i+1, len(time), title="Wave solver progress")
+            u[PAST] = u[PRESENT] 
+            u[PRESENT] = u[FUTURE] 
+            u[FUTURE] = (-self.delta_t**2 * A) @ u[PRESENT] - u[PAST] + 2*u[PRESENT]
+
+            D_fine[i] = self.xp.transpose(b) @ u[PRESENT]
+            D_fine[i] = 0.5*(D_fine[i].T + D_fine[i])
 
             if (i % nts) == 0:
                 index = int(i/nts)
-                D[index] = self.xp.transpose(b) @ u[1]
+                D[index] = self.xp.transpose(b) @ u[PRESENT]
                 D[index] = 0.5*(D[index].T + D[index])
+                
+                U_0[index,:,:] = cupy.asnumpy(u[PRESENT][self.imaging_region_indices])
 
-                count_storage_D += 1
-                #print(f"Count D = {count_storage_D}")
+        self._end_progress_bar()
+        U_0.flush()
 
-                if i <= self.setup.N_t*nts-1:
-                    U_0[:,:,index] = u[1][self.imaging_region_indices]
-
-                    #print(f"U_0 current timestep: {index+1}/{self.N_t}")
-
-                    count_storage_U_0 += 1
-
-
-        sys.stdout.write("\n")
-        # self.xp.save(self.data_folder + "U_0.npy", U_0)
-        # print(f"Saved U_0 of shape {U_0.shape}")
-        
-        U_0 = self.xp.reshape(U_0, (self.setup.N_x_im * self.setup.N_y_im, self.setup.N_s * self.setup.N_t),order='F')
-
-        #print(f"Count D = {count_storage_D}")
-        #print(f"Count stage U_0 = {count_storage_U_0}")
-        return D, U_0
-
-
-    def __print_benchmark_progress(self, progress, max, progress_bar_length=40):
-        title = f"\rWave solver progress: {progress:5}/{max:5}: "
-        success_rate = f" {(progress/max)*100:3.2f}%"
-        number_of_progress_indicators = int(progress * progress_bar_length // (max))
-
-        sys.stdout.write(title + "[" + number_of_progress_indicators*"#" + (progress_bar_length - number_of_progress_indicators)*"-" + "]" + success_rate)
+        return D, D_fine
 
 
 def main():
-    N_t = 10
     use_gpu = False
 
     for i, arg in enumerate(sys.argv):
-        if arg == '-Nt':
-            N_t = int(sys.argv[i+1])
-        elif arg == '-gpu':
+        if arg == '-gpu':
             use_gpu = True
 
-    sim_setup = SimulationSetup(N_t=N_t)
-    solver = WaveSolver(sim_setup, use_gpu)
-    solver.calculate_U0_and_D0()
-    
-    # test = solver.import_sources()
-    # print(test.shape)
+    sim_setup = SimulationSetup(N_t=65,
+                                N_x_im=140,
+                                N_y_im=155,
+                                O_x=25,
+                                O_y=180)
+    exec_setup = ExecutionSetup(
+        gpu=use_gpu,
+        precision='float64')
+    solver = WaveSolver(sim_setup, exec_setup)
+    solver.calculate_U0_D0()
+    # solver.calculate_U_D("old_fwd/fractures/circle.npy")
+
 
 if __name__ == "__main__":
     main()
