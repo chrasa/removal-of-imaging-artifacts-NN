3a9979b68e3c5c55406a530d3325559ac115e05a
Christoph Asam <asam.chr@gmail.com>
2023-04-11 14:16:17 +0200

    Remove matplotlib dependency form FractureGenerator

--- uppmax_test.sh
+++ uppmax_test.sh
@@ -2,9 +2,9 @@
 #SBATCH -M snowy
 #SBATCH -A snic2022-22-1060
 #SBATCH -p node -N 1
-#SBATCH -t 1:00:00
+#SBATCH -t 10:00
 #SBATCH --qos=short
-#SBATCH --gpus=1
+#SBATCH --gres=gpu:1
 #SBATCH --gpus-per-node=1
 #SBATCH -J generate_images_test
 #SBATCH -D ./
@@ -13,6 +13,8 @@ module load python3/3.9.5
 module load python_ML_packages/3.9.5-gpu
 
 source venv/bin/activate
+
+nvidia-smi
 
 echo "Generate images..."
 
