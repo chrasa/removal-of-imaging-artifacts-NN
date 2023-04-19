145a8ee216a6c50af4bb738dc20032862853ba8f
Christoph Asam <asam.chr@gmail.com>
2023-04-19 16:53:34 +0200

    Added support for plotting series plots

--- plot_rtm_rom.py
+++ plot_rtm_rom.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from os import path
 from setup import SimulationSetup
+from benchmark import ProgressBar
 
 def get_imaging_region_indices(setup: SimulationSetup):
     im_y_indices = range(setup.O_y, setup.O_y+setup.N_y_im)
@@ -43,49 +44,67 @@ def plot_rtm_image(I_rom_path, I_rtm_path, fracture_path, setup: SimulationSetup
 
     plt.show()
 
-def plot_rtm_image_from_single_file(data_path, image_idx, setup: SimulationSetup):
-    training_data = np.load(data_path)
-
+def plot_rtm_image_from_single_file(training_data, setup: SimulationSetup, output_file=False):
     f, (ax1, ax2, ax3) = plt.subplots(1, 3)
 
-    rom_image = np.squeeze(training_data[image_idx, 2])
-    rom_image = rom_image.reshape(setup.N_y_im, setup.N_x_im)
+    rom_image = np.squeeze(training_data[2])
+    rom_image = rom_image.reshape(setup.N_x_im, setup.N_y_im)
     clipping_max_rom = np.max(np.abs(rom_image))
 
     ax1.set_title("ROM Image")
     ax1.imshow(rom_image.T, vmin=-clipping_max_rom, vmax=clipping_max_rom)
 
-    rtm_image = np.squeeze(training_data[image_idx, 1])
-    rtm_image = rtm_image.reshape(setup.N_y_im, setup.N_x_im)
+    rtm_image = np.squeeze(training_data[1])
+    rtm_image = rtm_image.reshape(setup.N_x_im, setup.N_y_im)
     clipping_max_rtm = np.max(np.abs(rtm_image))
 
     ax2.set_title("RTM Image")
     ax2.imshow(rtm_image.T, vmin=-clipping_max_rtm, vmax=clipping_max_rtm)
 
-    fracture_image = np.squeeze(training_data[image_idx, 0])
-    fracture_image = np.reshape(fracture_image, (setup.N_y_im, setup.N_x_im))
+    fracture_image = np.squeeze(training_data[0])
+    fracture_image = np.reshape(fracture_image, (setup.N_x_im, setup.N_y_im))
 
     ax3.set_title("Fracture Image")
     ax3.imshow(fracture_image.T)
 
-    plt.show()
+    if output_file:
+        plt.savefig(output_file, dpi=150)
+        plt.close()
+    else:
+        plt.show()
 
 if __name__ == "__main__":
     idx = 0
+    training_data_path = "./data/training_data.npy"
+    series_plot = False
+    output_path = False
 
     for i, arg in enumerate(sys.argv):
         if arg == '-n':
             idx = int(sys.argv[i+1])
+        elif arg == '-p':
+            training_data_path = sys.argv[i+1]
+        elif arg == '-series':
+            series_plot = True
+        elif arg == '-out':
+            output_path = sys.argv[i+1]
 
-    I_rom_path = "./data/I_ROM.npy"
-    I_rtm_path = "./data/I_RTM.npy"
-    fracture_path = "./old_fwd/fractures/circle.npy"
-    setup = SimulationSetup(
-        N_y_im=155, 
-        N_x_im=140,
-        O_x=25,
-        O_y=180
-    )
+    setup = SimulationSetup()
+    training_data = np.load(training_data_path)
 
+    if series_plot:
+        if not output_path:
+            raise Exception("No output path provided")
+        n_training_samples = training_data.shape[0]
+        progress_bar = ProgressBar()
+        for i in range(n_training_samples):
+            progress_bar.print_progress(i+1, n_training_samples)
+            plot_rtm_image_from_single_file(training_data[i], setup, f"{output_path}im{i:04}.jpg")
+        progress_bar.end()
+    else:
+        plot_rtm_image_from_single_file(training_data[idx], setup)
+
+    # I_rom_path = "./data/I_ROM.npy"
+    # I_rtm_path = "./data/I_RTM.npy"
+    # fracture_path = "./old_fwd/fractures/circle.npy"
     # plot_rtm_image(I_rom_path, I_rtm_path, fracture_path, setup)
-    plot_rtm_image_from_single_file("./data/training_data.npy", idx, setup)
