69cbecc9f34aaa8d3640db86d78d1e1cb748414e
Christoph Asam <asam.chr@gmail.com>
2023-04-20 13:14:24 +0200

    Extend time limit

--- generate_training_data.sh
+++ generate_training_data.sh
@@ -2,10 +2,10 @@
 #SBATCH -M snowy
 #SBATCH -A snic2022-22-1060
 #SBATCH -p core -N 1
-#SBATCH -t 12:00:00
+#SBATCH -t 14:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --gpus-per-node=1
-#SBATCH -J generate_images_test
+#SBATCH -J generate_training_data
 #SBATCH -D ./
 
 module load python3/3.9.5
