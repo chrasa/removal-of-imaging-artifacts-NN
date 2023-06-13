736c03adb8611fddefc39f5aa6dc0f234c825f47
Christoph Asam <asam.chr@gmail.com>
2023-06-13 11:45:54 +0200

    Script for normalizing data

---
+++ training_data/normalize_data.py
@@ -0,0 +1,85 @@
+import sys
+import numpy as np
+
+class DataNormalizer:
+    def __init__(self, n) -> None:
+        self.training_data_file_name = "training_data.npy"
+        print("Loading training data...")
+        self.training_data = np.memmap(self.training_data_file_name, np.float64, 'r', shape=(n, 3, 350*180))
+
+    def normalize_data(self):
+        self.__remove_background_velocity()
+
+        rom_data = self.training_data[:,2,:]
+        rom_max = np.max(rom_data)
+        print(f"ROM max value: {rom_max}")
+        rom_min = np.min(rom_data)
+        print(f"ROM min value: {rom_min}")
+        rom_abs_max = np.max(np.abs([rom_max, rom_min]))
+        self.training_data[:,2,:] = rom_data/rom_abs_max
+        
+        rtm_data = self.training_data[:,1,:]
+        rtm_max = np.max(rtm_data)
+        print(f"RTM max value: {rtm_max}")
+        rtm_min = np.min(rtm_data)
+        print(f"RTM min value: {rtm_min}")
+        rtm_abs_max = np.max(np.abs([rtm_max, rtm_min]))
+        self.training_data[:,1,:] = rtm_data/rtm_abs_max
+
+        frac_data = self.training_data[:,0,:]
+        frac_max = np.max(frac_data)
+        print(f"Fracture max value: {frac_max}")
+        frac_min = np.min(frac_data)
+        print(f"Fracture min value: {frac_min}")
+        frac_abs_max = np.max(np.abs([frac_max, frac_min]))
+        self.training_data[:,0,:] = frac_data/frac_abs_max
+        self.training_data.flush()
+
+
+        print("New max and min values")
+        rom_max = np.max(self.training_data[:,2,:])
+        print(f"ROM max value: {rom_max}")
+        rom_min = np.min(self.training_data[:,2,:])
+        print(f"ROM min value: {rom_min}")
+        
+        rtm_max = np.max(self.training_data[:,1,:])
+        print(f"RTM max value: {rtm_max}")
+        rtm_min = np.min(self.training_data[:,1,:])
+        print(f"RTM min value: {rtm_min}")
+
+        frac_max = np.max(self.training_data[:,0,:])
+        print(f"Fracture max value: {frac_max}")
+        frac_min = np.min(self.training_data[:,0,:])
+        print(f"Fracture min value: {frac_min}")
+
+    def remove_invalid_data(self):
+        frac_data = self.training_data[:,0,:]
+        invalid_data_idx = []
+        for i in range(frac_data.shape[0]):
+            if np.min(frac_data[i]) < 10:
+                print(f"Images with index {i} are invalid")
+                invalid_data_idx.append(i)
+
+        self.training_data = np.delete(self.training_data, invalid_data_idx, axis=0)
+        print(f"Shape of cleaned training data: {self.training_data.shape}")
+
+    def save_training_data(self):
+        np.save("training_data_normalized.npy", self.training_data)
+
+    def __remove_background_velocity(self):
+        self.training_data[:,0,:] = self.training_data[:,0,:] - 1000.0
+
+
+
+def main():
+    for i, arg in enumerate(sys.argv):
+        if arg == '-n':
+            n_images = int(sys.argv[i+1])
+    data_normalizer = DataNormalizer(n_images)
+    data_normalizer.remove_invalid_data()
+    data_normalizer.normalize_data()
+    data_normalizer.save_training_data()
+
+
+if __name__ == "__main__":
+    main()
