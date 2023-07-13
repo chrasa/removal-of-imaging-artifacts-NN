7ed4122660f841f0fc5f839236c4dd2ec40f04fa
Christoph Asam <asam.chr@gmail.com>
2023-07-13 12:46:49 +0200

    Train UNet only

--- train_nn_rtm.sh
+++ train_nn_rtm.sh
@@ -3,7 +3,7 @@
 #SBATCH -A snic2022-22-1060
 #SBATCH -p core
 #SBATCH -n 4
-#SBATCH -t 16:00:00
+#SBATCH -t 8:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --gpus-per-node=1
 #SBATCH -J train_nn_rtm
@@ -20,23 +20,23 @@ echo "Train networks..."
 
 ##### RTM #####
 # ConvAuto
-python3 train_nn.py rtm ConvAuto sobel -stride 2 -nimages 4080
-python3 train_nn.py rtm ConvAuto sobel -stride 5 -nimages 4080
-python3 train_nn.py rtm ConvAuto ssim -stride 2 -nimages 4080
-python3 train_nn.py rtm ConvAuto ssim -stride 5 -nimages 4080
+# python3 train_nn.py rtm ConvAuto sobel -stride 2 -nimages 4080
+# python3 train_nn.py rtm ConvAuto sobel -stride 5 -nimages 4080
+# python3 train_nn.py rtm ConvAuto ssim -stride 2 -nimages 4080
+# python3 train_nn.py rtm ConvAuto ssim -stride 5 -nimages 4080
 
 # ConvNN
-python3 train_nn.py rtm ConvNN sobel -stride 5 -nimages 4080
-python3 train_nn.py rtm ConvNN ssim -stride 5 -nimages 4080
+# python3 train_nn.py rtm ConvNN sobel -stride 5 -nimages 4080
+# python3 train_nn.py rtm ConvNN ssim -stride 5 -nimages 4080
 
 # ResNet
-python3 train_nn.py rtm ResNet sobel -stride 2 -nimages 4080
-python3 train_nn.py rtm ResNet sobel -stride 5 -nimages 4080
-python3 train_nn.py rtm ResNet ssim -stride 2 -nimages 4080
-python3 train_nn.py rtm ResNet ssim -stride 5 -nimages 4080
+# python3 train_nn.py rtm ResNet sobel -stride 2 -nimages 4080
+# python3 train_nn.py rtm ResNet sobel -stride 5 -nimages 4080
+# python3 train_nn.py rtm ResNet ssim -stride 2 -nimages 4080
+# python3 train_nn.py rtm ResNet ssim -stride 5 -nimages 4080
 
 # UNet
-python3 train_nn.py rtm UNet sobel -stride 2 -nimages 4080
+# python3 train_nn.py rtm UNet sobel -stride 2 -nimages 4080
 python3 train_nn.py rtm UNet sobel -stride 5 -nimages 4080
 python3 train_nn.py rtm UNet ssim -stride 2 -nimages 4080
 python3 train_nn.py rtm UNet ssim -stride 5 -nimages 4080
